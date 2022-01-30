1747d03fc4dd96d6bd87f7995aaa4e760d482a7e
shipley7 <ashiple4@uwo.ca>
2022-01-30 14:11:19 -0500

    Attempting Navigation and Level 1

--- App.js
+++ App.js
@@ -1,14 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from 'react-native-web';
 
-export default function App() {
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to First Level"
+        onPress={() => {navigation.navigate('FirstLevel', {
+            numbers: generateRandomNumbers(1),
+          })
+        }}
+      />
     </View>
   );
 }
+
+function Level1({ route, navigation }) {
+  const { numbers } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> First Level</Text>
+      <Text>Numbers: {JSON.stringify(numbers)}</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="FirstLevel" component={Level1} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +58,29 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function generateRandomNumbers(level){
+  var arr = [];
+
+  if (level==1 || level==2 || level==3){
+    while(arr.length < 10){
+      var r = Math.floor(Math.random() * 20) + 1;
+      if(arr.indexOf(r) === -1) arr.push(r);
+  }
+  }
+  else if (level==4) {
+    while(arr.length < 20){
+      var r = Math.floor(Math.random() * 50) + 1;
+      if(arr.indexOf(r) === -1) arr.push(r);
+  }
+  }
+  else if (level==5) {
+    while(arr.length < 50){
+      var r = Math.floor(Math.random() * 100) + 1;
+      if(arr.indexOf(r) === -1) arr.push(r);
+  }
+  }
+  return arr;
+}
+
+
