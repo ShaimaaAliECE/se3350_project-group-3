8a10cffe187a6d7c652b222d224de9899d5ba16f
Hyunseung Kim <hyunseungkim@dyn172-30-143-195.wireless.uwo.pri>
2022-01-31 16:34:12 -0500

    folder setup + level 1 page code example

--- App.js
+++ App.js
@@ -4,44 +4,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Button } from 'react-native-web';
 
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to First Level"
-        onPress={() => {navigation.navigate('FirstLevel', {
-            numbers: generateRandomNumbers(1),
-          })
-        }}
-      />
-    </View>
-  );
-}
-
-function Level1({ route, navigation }) {
-  const { numbers } = route.params;
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text> First Level</Text>
-      <Text>Numbers: {JSON.stringify(numbers)}</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Home')}
-      />
-    </View>
-  );
-}
+import HomeScreen from './src/pages/Home';
+import FirstLevelScreen from './src/pages/FirstLevel';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName = "Home">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="FirstLevel" component={Level1} />
+        <Stack.Screen name="FirstLevel" component={FirstLevelScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -59,28 +32,6 @@ const styles = StyleSheet.create({
   },
 });
 
-function generateRandomNumbers(level){
-  var arr = [];
 
-  if (level==1 || level==2 || level==3){
-    while(arr.length < 10){
-      var r = Math.floor(Math.random() * 20) + 1;
-      if(arr.indexOf(r) === -1) arr.push(r);
-  }
-  }
-  else if (level==4) {
-    while(arr.length < 20){
-      var r = Math.floor(Math.random() * 50) + 1;
-      if(arr.indexOf(r) === -1) arr.push(r);
-  }
-  }
-  else if (level==5) {
-    while(arr.length < 50){
-      var r = Math.floor(Math.random() * 100) + 1;
-      if(arr.indexOf(r) === -1) arr.push(r);
-  }
-  }
-  return arr;
-}
 
 
