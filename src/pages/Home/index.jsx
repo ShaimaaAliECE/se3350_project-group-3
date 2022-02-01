be88854338ffdf01e88d02c4ff7fad0517d9c431
maariashaikh <bshaikh5@uwo.ca>
2022-02-01 14:36:08 -0500

    Added login button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -28,11 +28,20 @@ function HomeScreen({ navigation }) {
   }
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ fontSize:25 }}>Please select an algorithm</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', padding: 200}}>
+          <View style={{ flexDirection: 'row' }}>
+          <View style={{ width: 1000 }} />
+          <View style={{ height: 40, }} />
+          <Button
+            title="Login Here"
+            onPress={() => navigation.navigate('Login')}
+          />
+        </View>
+      <Text style={{ fontSize: 25 }}>Please select an algorithm</Text>
       <View style={{ height: 120 }} />
+
       <View style={{ flexDirection: 'row' }}>
-      {/* Creating buttons on homepage to guide user to the algorithm they'd like*/ }
+        {/* Creating buttons on homepage to guide user to the algorithm they'd like*/}
         <Button
           title="Merge Sort Algorithm"
           onPress={() => {
@@ -50,9 +59,12 @@ function HomeScreen({ navigation }) {
             })
           }}
         />
+        
       </View>
 
     </View>
+
+
   );
 }
 
