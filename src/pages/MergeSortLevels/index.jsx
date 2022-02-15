b4904caaf61087431044d8d0d08d094dc5dbdbcf
shipley7 <ashiple4@uwo.ca>
2022-02-15 18:25:59 -0500

    Implementing Sounds for Levels

--- src/pages/MergeSortLevels/index.jsx
+++ src/pages/MergeSortLevels/index.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { Audio } from 'expo-av';
+
 
 function MergeSortLevels({ navigation }) {
+  const [sound, setSound] = React.useState();
+
+  async function playCorrectSound() {
+    const { sound } = await Audio.Sound.createAsync(
+       require('../../../assets/correct.mp3')
+    );
+    setSound(sound);
+    await sound.playAsync(); 
+  }
+
+  async function playIncorrectSound() {
+    const { sound } = await Audio.Sound.createAsync(
+       require('../../../assets/incorrect.mp3')
+    );
+    setSound(sound);
+    await sound.playAsync(); 
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+  
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Merge Sort Levels</Text>
@@ -30,16 +58,12 @@ function MergeSortLevels({ navigation }) {
        <View style={{ height: 20 }} />
         <Button
         title="Level 4"
-        onPress={() => {
-          navigation.navigate('FourthLevel')
-        }}
+        onPress={playCorrectSound}
       />
        <View style={{ height: 20 }} />
         <Button
         title="Level 5"
-        onPress={() => {
-          navigation.navigate('FifthLevel')
-        }}
+        onPress={playIncorrectSound}
       />
        <View style={{ height: 20 }} />
       <Button
