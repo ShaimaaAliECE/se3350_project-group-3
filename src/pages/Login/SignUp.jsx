2fced79a0141f5110357a1c481683552385c9934
shipley7 <ashiple4@uwo.ca>
2022-03-25 18:25:30 -0400

    Fix merge issue?
    Trying to fix a merging issue

--- src/pages/Login/SignUp.jsx
+++ src/pages/Login/SignUp.jsx
@@ -1,16 +1,51 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
-const fs = require('fs')
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 
-export default function SignUp() {
-   
+const setUser = async (username, password) => {
+    let value = {username, password}
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('user', jsonValue)
+    } catch(e) {
+      // save error
+    }
+  
+    console.log('Done.')
+    console.log(value)
+  }
+
+const getUser = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('user')
+      return jsonValue != null ? JSON.parse(jsonValue) : null
+    } catch(e) {
+      // read error
+    }
+  
+  
+  }
+
+export default function SignUp({navigation}) {
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     
     return (
+      
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{width:"80%", textAlign:"center", float: "left"}}>
+        <h1>Sign Up</h1>
+      </div>
+      
+      <Button 
+      title="Login"
+      onPress={() => {
+        navigation.navigate("Login")
+      }}/>
+      
         <Text>Username</Text>
         <TextInput style={{ borderWidth: 1, borderColor: 'black'}} value={username} onChangeText={(text) => setUsername(text)} />
         
@@ -19,8 +54,15 @@ export default function SignUp() {
         <Button
             title="Sign Up"
             onPress={() => {
-                fs.writeFile('../LoginJSON.json', JSON.stringify(username))
-                fs.writeFile('../LoginJSON.json', JSON.stringify(password))
+                setUser(username, password)
+         
+            }       
+        }
+        />
+        <Button
+            title="Get"
+            onPress={() => {
+                console.log(getUser())
          
             }       
         }
